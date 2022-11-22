0b2fcb35760f38527635696e08ff41373b598f47
Paulo-HSO <91340154+Paulo-HSO@users.noreply.github.com>
2022-11-22 11:39:19 -0300

    Add files via upload

--- 2DM/app.py
+++ 2DM/app.py
@@ -1,12 +1,27 @@
 import pywhatkit
 import keyboard
 import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from webdriver_manager.chrome import ChromeDriverManager 
 from datetime import datetime
 
-contatos = ['+5511942419921', '+5511984207071']
+driver = webdriver.Chrome(ChromeDriverManager().install())
+driver.get('https://web.whatsapp.com/')
+time.sleep(30)
 
-while len(contatos) >= 1:
-    pywhatkit.sendwhatmsg(contatos[0], 'Ignore esta mensagem', datetime.now().hour, datetime.now().minute + 1)
-    del contatos [0]
-    time.sleep(60)
-    keyboard.press_and_release('ctrl + w')
+contatos = ['Chatbot', 'NADA']
+mensagem = 'KAVALO'
+
+def buscar_contato(contato):
+    campo_pesquisa = driver.find_element("xpath",'//div[contains(@class,"copyable-text selectable-text")]')
+    time.sleep(3)
+    campo_pesquisa.click()
+    campo_pesquisa.send_keys(contato)
+    campo_pesquisa.send_keys(Keys.ENTER)
+    
+    
+for contato in contatos:
+    buscar_contato(contato)
+    enviar_mensagem(mensagem)
+   
